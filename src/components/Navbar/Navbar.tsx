c45113d168cb5554c5d09ba0201dea7e2e7863f3
Ayush Singh <asayushsingh80@gmail.com>
2024-12-06 00:24:44 +0530

    added hamburger menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import "./index.css";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar__logo">
@@ -9,17 +16,23 @@ function Navbar() {
         </div>
         <div className="navbar__logo-text">ProVital</div>
       </div>
-      <ul className="navbar__menu">
-        <li className="navbar__item">Home</li>
-        <li className="navbar__item">Home</li>
-        <li className="navbar__item">Home</li>
-        <li className="navbar__item">Home</li>
-        <li className="navbar__item">Home</li>
-        <li className="navbar__item">Home</li>
+      <ul
+        className={`navbar__menu ${isMenuOpen ? "navbar__menu--active" : ""}`}
+      >
+        <li className="navbar__item">List your practice</li>
+        <li className="navbar__item">For Employers</li>
+        <li className="navbar__item">Courses</li>
+        <li className="navbar__item">Books</li>
+        <li className="navbar__item">Speakers</li>
+        <li className="navbar__item">Doctors</li>
         <li className="navbar__item">Home</li>
       </ul>
-      <div className="navbar__menu-icon">
-        <img src="/icons/hamburger-icon.svg" />
+      <div className="navbar__menu-icon" onClick={handleMenu}>
+        {!isMenuOpen ? (
+          <img src="/icons/hamburger-icon-open.svg" />
+        ) : (
+          <img src="icons/hamburger-icon-close.svg" />
+        )}
       </div>
     </nav>
   );
