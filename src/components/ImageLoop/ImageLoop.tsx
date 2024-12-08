33051ca9812566bcc4e120188569423a34358541
Ayush Singh <asayushsingh80@gmail.com>
2024-12-08 17:40:45 +0530

    ui: main content

--- src/components/ImageLoop/ImageLoop.tsx
+++ src/components/ImageLoop/ImageLoop.tsx
@@ -11,6 +11,19 @@ const images = [
   "/images/image-8.png",
 ];
 
+const verticalImagesCol1 = [
+  "/images/image-1.png",
+  "/images/image-2.png",
+  "/images/image-3.png",
+  "/images/image-4.png",
+];
+
+const verticalImagesCol2 = [
+  "/images/image-5.png",
+  "/images/image-6.png",
+  "/images/image-7.png",
+  "/images/image-8.png",
+];
 function ImageLoop() {
   return (
     <div className="loop-container">
@@ -21,6 +34,30 @@ function ImageLoop() {
           </div>
         ))}
         {images.map((image, index) => (
+          <div className="img__slide" key={index * 8}>
+            <img src={image} alt={`${image}-${index}`} />
+          </div>
+        ))}
+      </div>
+      <div className="img__slide-vertical-track">
+        {verticalImagesCol1.map((image, index) => (
+          <div className="img__slide" key={index}>
+            <img src={image} alt={`${image}-${index}`} />
+          </div>
+        ))}
+        {verticalImagesCol1.map((image, index) => (
+          <div className="img__slide" key={index * 8}>
+            <img src={image} alt={`${image}-${index}`} />
+          </div>
+        ))}
+      </div>
+      <div className="img__slide-vertical-track">
+        {verticalImagesCol2.map((image, index) => (
+          <div className="img__slide" key={index}>
+            <img src={image} alt={`${image}-${index}`} />
+          </div>
+        ))}
+        {verticalImagesCol2.map((image, index) => (
           <div className="img__slide" key={index * 8}>
             <img src={image} alt={`${image}-${index}`} />
           </div>
